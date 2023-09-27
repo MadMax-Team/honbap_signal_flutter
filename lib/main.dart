a0a4b1591ebb55ed681af082d23b3e570b013597
wjlee611 <wjlee611@gmail.com>
2023-09-27 19:38:49 +0900

    fcm cubit implement

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:honbap_signal_flutter/app.dart';
 import 'package:honbap_signal_flutter/bloc/auth/authentication/authentication_bloc.dart';
 import 'package:honbap_signal_flutter/bloc/splash/splash_bloc.dart';
+import 'package:honbap_signal_flutter/cubit/fcm_cubit.dart';
 import 'package:honbap_signal_flutter/cubit/user_cubit.dart';
 import 'package:honbap_signal_flutter/firebase_options.dart';
 import 'package:honbap_signal_flutter/repository/honbab/auth/auth_repository.dart';
@@ -15,36 +16,61 @@ import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:path_provider/path_provider.dart';
 import 'apis/kakao_api_key.dart';
 
+RemoteMessage? bgMessage;
+
 void main() async {
+  // flutter ensure initialized
   WidgetsFlutterBinding.ensureInitialized();
 
+  // initialize all dependencies
+  await initializeDependencies();
+
+  // listening fcm from background
+
+  FirebaseMessaging.onBackgroundMessage(_onBackgroundMessage);
+
+  // get fcm token
+  String? fcmToken = await FirebaseMessaging.instance.getToken();
+  print('FCM Token: $fcmToken');
+
+  runApp(MyApp(
+    fcmToken: fcmToken ?? '',
+    bgMessage: bgMessage,
+  ));
+}
+
+Future<void> _onBackgroundMessage(RemoteMessage message) async {
+  bgMessage = message;
+}
+
+Future<void> initializeDependencies() async {
   // Firebase initialize
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  // hydrated_bloc initialize
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+  // Kakao SDK initialize
+  KakaoSdk.init(nativeAppKey: KAKAO_API_KEY);
+  // set FCM permission
   FirebaseMessaging.instance.requestPermission(
     badge: true,
     alert: true,
     sound: true,
   );
-
-  // Kakao SDK initialize
-  KakaoSdk.init(nativeAppKey: KAKAO_API_KEY);
-
-  // for hydrated_bloc
-  HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: await getApplicationDocumentsDirectory(),
-  );
-
-  String? fcmToken = await FirebaseMessaging.instance.getToken();
-  print(fcmToken);
-
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String fcmToken;
+  final RemoteMessage? bgMessage;
+
+  const MyApp({
+    super.key,
+    required this.fcmToken,
+    this.bgMessage,
+  });
 
   // This widget is the root of your application.
   @override
@@ -72,6 +98,9 @@ class MyApp extends StatelessWidget {
           ),
           BlocProvider(
             create: (context) => SplashBloc(),
+          ),
+          BlocProvider(
+            create: (context) => FCMCubit(fcmToken, bgMessage),
           ),
         ],
         child: const App(),
