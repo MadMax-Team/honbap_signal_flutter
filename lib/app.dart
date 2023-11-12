b882dc66c424ad85d148c12b56f78c325aa7a0a9
wjlee611 <wjlee611@gmail.com>
2023-11-12 23:14:13 +0900

    fix timezone error

--- lib/app.dart
+++ lib/app.dart
@@ -23,7 +23,6 @@ import 'package:honbap_signal_flutter/screens/splash/splash_page.dart';
 import 'bloc/chat/chat_list/chat_list_bloc.dart';
 import 'bloc/home/get_signal_apply/home_signal_apply_bloc.dart';
 import 'bloc/home/get_signal_applyed/home_signal_applyed_bloc.dart';
-import 'bloc/signal/signal_state_bloc.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -148,8 +147,8 @@ class _AppState extends State<App> {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
-        Locale('en'),
         Locale('ko'),
+        Locale('en'),
       ],
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
