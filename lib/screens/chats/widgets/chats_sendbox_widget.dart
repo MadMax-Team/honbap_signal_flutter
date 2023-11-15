64d375ac9cc3daac4a43c9085a3516323966c69a
wjlee611 <wjlee611@gmail.com>
2023-11-15 21:18:17 +0900

    change chats sendbox design

--- lib/screens/chats/widgets/chats_sendbox_widget.dart
+++ lib/screens/chats/widgets/chats_sendbox_widget.dart
@@ -10,17 +10,25 @@ class ChatsSendboxWidget extends StatefulWidget {
 
 class _ChatsSendboxWidgetState extends State<ChatsSendboxWidget> {
   late final TextEditingController _textEditingController;
+  late final FocusNode _textFocusNode;
 
   @override
   void initState() {
     super.initState();
     _textEditingController = TextEditingController();
+    _textFocusNode = FocusNode();
   }
 
   @override
   void dispose() {
     _textEditingController.dispose();
+    _textFocusNode.dispose();
     super.dispose();
+  }
+
+  void _sendMessage() {
+    print('send: ${_textEditingController.value.text}');
+    _textEditingController.clear();
   }
 
   @override
@@ -44,36 +52,33 @@ class _ChatsSendboxWidgetState extends State<ChatsSendboxWidget> {
         ],
         color: Colors.white,
       ),
-      child: Row(
-        children: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.add,
-              color: Theme.of(context).primaryColor,
-            ),
-          ),
-          Container(
-            width: Sizes.size1,
-            height: Sizes.size28,
-            color: Colors.grey.shade300,
-          ),
-          Expanded(
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: Sizes.size10),
-              child: TextFormField(
-                controller: _textEditingController,
-              ),
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
+      child: TextFormField(
+        controller: _textEditingController,
+        focusNode: _textFocusNode,
+        onTapOutside: (event) => FocusScope.of(context).unfocus(),
+        onFieldSubmitted: (value) {
+          _sendMessage();
+          _textFocusNode.requestFocus();
+        },
+        textInputAction: TextInputAction.send,
+        style: const TextStyle(
+          fontSize: Sizes.size14,
+          fontWeight: FontWeight.w400,
+          color: Colors.black87,
+        ),
+        textAlignVertical: TextAlignVertical.center,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.only(left: Sizes.size20),
+          suffixIcon: IconButton(
+            onPressed: _sendMessage,
+            splashRadius: 0.001,
             icon: Icon(
               Icons.arrow_circle_right,
               color: Theme.of(context).primaryColor,
             ),
           ),
-        ],
+        ),
       ),
     );
   }
